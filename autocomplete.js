db2754a181406268560533321613a1a91efb67e3
sandspirit2 <momom_2011@hotmail.com>
2019-07-04 02:53:44 +0200

    this is the commit after free hosting of json object for xml request success

--- autocomplete.js
+++ autocomplete.js
@@ -1,18 +1,30 @@
-$("#tags").autocomplete({
-    minLength: 2,
-    source: function(request, response) {
-        response($.map(mydata, function(obj, key) {
+var jsonUrl = 'https://api.myjson.com/bins/141r3r';
+var request = new XMLHttpRequest();
+request.open('GET', jsonUrl);
+request.responseType = 'json';
+request.send();
 
-            var name = obj.name.toUpperCase();
 
-            if (name.match(request.term.toUpperCase())) {
-                return {
-                    label: obj.name,
-                    value: obj.name
+request.onload = function() {
+    var mydata = request.response;
+
+
+    $("#tags").autocomplete({
+        minLength: 2,
+        source: function(request, response) {
+            response($.map(mydata, function(obj, key) {
+
+                var name = obj.name.toUpperCase();
+
+                if (name.match(request.term.toUpperCase())) {
+                    return {
+                        label: obj.name,
+                        value: obj.name
+                    }
                 }
-            }
-        }));
-    }
+            }));
+        }
 
 
-});
+    });
+}
